a9fbe48cab589e125bcc7ffddf517ff71f2e0c9c
Infy <kesarinandankumar2001@gmail.com>
2024-06-04 06:42:56 +0530

    Nandan's changes

--- src/Components/Homepage/Header.jsx
+++ src/Components/Homepage/Header.jsx
@@ -1,38 +1,58 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import './Header.css';
+import React from "react";
+import PropTypes from "prop-types";
+import { Container, Row, Col, Nav, Navbar } from "react-bootstrap";
+import "./Header.css";
 
-const Header = ({ className = '' }) => {
+const Header = ({ className = "" }) => {
   return (
-    <div>
-    <div className={`header ${className}`}>
-      <div className="headerChild" />
-      <img className="maskGroupIcon" alt="" src="/mask-group@2x.png" />
-      <img className="headerItem" alt="" src="/group-4.svg" />
-      <img className="headerInner" alt="" src="/group-3.svg" />
-      <div className="rectangleParent">
-        <div className="groupChild" />
-        <img
-          className="magnifyingGlass1Icon"
-          alt=""
-          src="/magnifyingglass-1.svg"
-        />
-        <div className="searchForSomething">Search for something</div>
-      </div>
-      <div className='project-name'>
-        <h1 style={{color: '#82659C',left: '1%'}}>MHOPHI</h1>
-      </div>
-      <div className="frameParent">
-        <div className="dashboardParent">
-          <div className="dashboard" style={{color: '#82659C'}}>Dashboard</div>
-          <img className="frameIcon" alt="" src="/frame.svg" />
-        </div>
-        <div className="ordersProductDetails">
-          Order’s Product Details
-        </div>
-      </div>
-    </div>
-    </div>
+    <Navbar
+      className={`header ${className}`}
+      bg="light"
+      expand="lg"
+      fixed="top"
+    >
+      <Container fluid>
+        <Navbar.Brand href="#">
+          <h1 className="project-name" style={{ color: "#82659C" }}>
+            MHOPHI
+          </h1>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: "100px" }}
+            navbarScroll
+          >
+            <Nav.Link href="#" className="dashboard">
+              Dashboard
+              <img className="frameIcon" alt="" src="/frame.svg" />
+            </Nav.Link>
+            <Nav.Link href="#">Order&apos;s Product Details</Nav.Link>
+          </Nav>
+          <div className="rectangleParent">
+            <div className="groupChild" />
+            <img
+              className="magnifyingGlass1Icon"
+              alt=""
+              src="/magnifyingglass-1.svg"
+            />
+            <div className="searchForSomething">Search for something</div>
+          </div>
+          <Nav>
+            <Nav.Link href="#">
+              <img className="headerItem" alt="" src="/group-4.svg" />
+            </Nav.Link>
+            <Nav.Link href="#">
+              <img className="headerInner" alt="" src="/group-3.svg" />
+            </Nav.Link>
+            <Nav.Link href="#">
+              <img className="maskGroupIcon" alt="" src="/mask-group@2x.png" />
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
